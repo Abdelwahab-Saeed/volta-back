38063cfe674a31532b7064aa6320af2509a2f343
Abdelwahab Saeed Younes <abdelwahabsaeed415@gmail.com>
2025-12-29 22:37:49 +0200

    Add Authentication with related tables

---
+++ database/migrations/2025_12_29_193551_add_fields_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            Schema::table('users', function (Blueprint $table) {
+            $table->date('date_of_birth')->nullable()->after('password');
+            $table->enum('role', ['admin', 'user'])->default('user')->after('date_of_birth');
+            $table->string('phone_number')->nullable()->after('role');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn(['date_of_birth', 'role', 'phone_number']);
+        });
+    }
+};
